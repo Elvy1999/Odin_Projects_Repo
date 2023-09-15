bff4d5a116c65e1c6e5f01daf3017a9b61d89f84
elvydecoo <elvydecoo99@gmail.com>
2023-09-15 03:09:32 -0400

    Finished adding a book to the books_content div from the information provided in the overlay

--- libraryApp/app.js
+++ libraryApp/app.js
@@ -2,6 +2,7 @@
 const add_book_btn = document.getElementById("add_book");
 const overlay_id = document.getElementById("overlay");
 const book_form = document.querySelector(".book-form");
+const books_content = document.querySelector(".books_content");
 
 
 
@@ -11,7 +12,7 @@ const book_form = document.querySelector(".book-form");
 
 
 
-
+let books_array = [];
 
 
 
@@ -35,17 +36,58 @@ add_book_btn.addEventListener("click", ()=>{
 
 book_form.addEventListener("submit",function(event){
     event.preventDefault(); // Prevents the form from submitting normally
-    const formElements = this.elements;
-    const formData = {}
+    const formElements = this.elements; // form elements
+    const formData = {} // create an object to store form data
     for(let i=0; i< formElements.length; i++){
-        const element = formElements[i];
+        const element = formElements[i]; // access the first element in the form
         if(element.type !== "submit"){
             formData[element.name] = element.type === "checkbox" ? element.checked : element.value;
         }
     }
+    formData["id"] = formData["book-name"]+formData["author"];
     console.log(formData)
-    
-
-
+    // resets form 
+    book_form.reset();
+    overlay.style.right = "-100%";
+    books_array.push(formData);
+    book_to_html(formData);
+    console.log(books_array);
 });
+
+function book_to_html(book){
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.id = book["id"];
+    let infoDiv = document.createElement("div");
+    infoDiv.classList.add("info");
+    let titleH2 = document.createElement("h2");
+    titleH2.classList.add("title");
+    titleH2.textContent = `Title: ${book["book-name"]}`;
+    infoDiv.appendChild(titleH2);
+    let authorH3 = document.createElement("h3");
+    authorH3.classList.add("author");
+    authorH3.textContent = `Author: ${book["author"]}`;
+    infoDiv.appendChild(authorH3);
+    let currentPageP = document.createElement("p");
+    currentPageP.classList.add("current-page");
+    currentPageP.textContent = `Current Page:${book["page-number"]}`;
+    infoDiv.appendChild(currentPageP);
+    bookDiv.appendChild(infoDiv);
+    let book_btnsDiv = document.createElement("div");
+    book_btnsDiv.classList.add("book_btns");
+    let readBtn = document.createElement("button");
+    readBtn.classList.add("read");
+    readBtn.textContent = "Read";
+    book_btnsDiv.appendChild(readBtn);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
+    deleteBtn.textContent = "Delete";
+    book_btnsDiv.appendChild(deleteBtn);
+    bookDiv.appendChild(book_btnsDiv);
+    let statusDiv = document.createElement("div");
+    statusDiv.classList.add("status");
+    statusDiv.textContent = book['read'] == true ? "Read" : "In Progress";
+    bookDiv.appendChild(statusDiv);
+    books_content.appendChild(bookDiv);
+}
 
