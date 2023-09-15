efeb9f8c4e02a86fca52c5864844070157c340fe
elvydecoo <elvydecoo99@gmail.com>
2023-09-15 17:17:10 -0400

    Finished adding the ability to dynamically update the books array, such as being able to update read status for a book as well as being able to delete a book from the array

--- libraryApp/app.js
+++ libraryApp/app.js
@@ -5,7 +5,10 @@ const book_form = document.querySelector(".book-form");
 const books_content = document.querySelector(".books_content");
 const all_books = books_content.querySelectorAll(".book");
 
-let books_array = [];
+let books_array = [{author: "Marcus Aurelius", book_name: "Meditations", "page-number": 7, "read":true, id:"book-1"}
+,{author: "James Clear", book_name: "Atomic Habits", "page-number": 25, "read":true,id:"book-2"}
+,{author: "Sherife Abdelmsddh", book_name: "Meditations", "page-number": 7, "read":true,id:"book-3"}
+];
 
 
 books_content.addEventListener("click", function(event){
@@ -13,21 +16,30 @@ books_content.addEventListener("click", function(event){
     if(readBtn){
         let book = event.target.closest(".book");
         let status = book.querySelector(".status");
+        let book_id = book.id;
+        let book_index = books_array.findIndex(obj => obj.id == book_id);
         if(status.textContent === "Completed"){
             status.textContent = "In Progress";
+            books_array[book_index]["read"] = false;
         }
         else{
             status.textContent = "Completed";
+            books_array[book_index]["read"] = true;
         }
+        console.log(books_array)
     }
-});
-
-books_content.addEventListener("click", function(event){
     let deleteBtn = event.target.closest(".delete");
     if(deleteBtn){
-        let book = event.target.closest(".book");
+        // propagates up the dom tree from the event that triggered it to find an element that has the 
+        // the class or id specified, in this case its looking for an element with the book class
+        let book = event.target.closest(".book"); 
+        let book_id = book.id; // get the id element of the div found with the class book
         book.remove();
+        let book_index = books_array.findIndex(obj => obj.id == book_id);
+        books_array.splice(book_index, 1);
+        console.log(books_array);
     }
+    
 });
 
 
@@ -58,14 +70,13 @@ book_form.addEventListener("submit",function(event){
             formData[element.name] = element.type === "checkbox" ? element.checked : element.value;
         }
     }
-    formData["id"] = formData["book-name"]+formData["author"];
-    console.log(formData)
+    formData["id"] = formData["book_name"]+formData["author"];
+
     // resets form 
     book_form.reset();
     overlay.style.right = "-100%";
     books_array.push(formData);
     book_to_html(formData);
-    console.log(books_array);
 });
 
 function book_to_html(book){
@@ -76,7 +87,7 @@ function book_to_html(book){
     infoDiv.classList.add("info");
     let titleH2 = document.createElement("h2");
     titleH2.classList.add("title");
-    titleH2.textContent = `Title: ${book["book-name"]}`;
+    titleH2.textContent = `Title: ${book["book_name"]}`;
     infoDiv.appendChild(titleH2);
     let authorH3 = document.createElement("h3");
     authorH3.classList.add("author");
