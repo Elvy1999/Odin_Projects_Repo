cec7dd90cd73465dab4e0e190b1eca831f524a00
elvydecoo <elvydecoo99@gmail.com>
2023-09-19 23:30:54 -0400

    added game buttons and pictures on the game board, for refrence

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the header elements
     const headerElements = document.querySelectorAll('header');
-
     // Apply transition effect after a short delay (adjust as needed)
     setTimeout(function() {
         headerElements.forEach(function(element) {
@@ -10,18 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }, 600); // Adjust the delay in milliseconds (0.5 seconds in this example)
 });
-
-
-const play_btn = document.querySelector('#play_btn');
-play_btn.addEventListener('click', () => {
-    console.log("I've been clicked");
-
-});
-
+const game_board_cells = document.querySelectorAll(".cell"); 
 const menu_container = document.querySelector('.menu-container');
 const game_container = document.querySelector('.game-container');
-const title = document.querySelector(".title");
-title.addEventListener("click", () => { 
+const play_btn = document.querySelector('#play_btn');
+const restart_btn = document.querySelector('.restart');
+play_btn.addEventListener('click', () => {switch_container();});
+const menu_btn = document.querySelector('.menu');
+menu_btn.addEventListener('click', () => {switch_container();});
+restart_btn.addEventListener('click', () => {
+    for(let cell of game_board_cells){
+        cell.innerHTML = "";
+    }
+});
+
+function switch_container(){
     if(game_container.classList.contains("active")){
         game_container.classList.remove("active");
         menu_container.classList.add("active");
@@ -32,4 +34,4 @@ title.addEventListener("click", () => {
         menu_container.classList.remove("active");
         document.body.style.overflow = 'hidden'; // Enable scrolling
     }
-});
+}
